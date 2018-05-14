edc9e1eda298f636ace2e683a1f91f314ce2804d
Pradnyesh Sawant <spradnyesh@gmail.com>
2018-05-14 10:47:22 +0530

    fix: solve-me-first-fp (0 mins)
    - was already solved

---
+++ src/hackerrank/solve_me_first_fp.clj
@@ -0,0 +1,14 @@
+(ns hackerrank.solve-me-first-fp)
+
+;; https://www.hackerrank.com/challenges/fp-solve-me-first/problem
+
+
+
+(defn solveMeFirst [x y]
+  (+ x y))
+
+
+(def a (read-line))
+(def b (read-line))
+
+(println (solveMeFirst (Integer/parseInt a) (Integer/parseInt b)))
